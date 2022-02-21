708a536573909ba5b1170cda14ad1be9fa1ee0d2
choinashil <choinashil@gmail.com>
2022-02-21 15:28:33 +0900

    config path alias

--- apps/next-app/pages/index.tsx
+++ apps/next-app/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Button from "@packages/shared/dist/src/components/Button";
+import Button from "@shared/components/Button";
 
 const Home: NextPage = () => {
   return (
--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "@packages/shared/dist/src/components/Button";
+import Button from "@shared/components/Button";
 
 function App() {
   return (
